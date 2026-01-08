7d3aceb89d5317b96b9a34ad1cb1916ef77b455a
Shubham <shubhaws74@gmail.com>
2026-01-08 23:48:42 +0530

    latest change

---
+++ src/string/ReverseEachWord.java
@@ -0,0 +1,10 @@
+package string;
+import java.util.stream.*;
+import java.util.*;
+public class ReverseEachWord {
+    /*2.Reverse Each Word in a String Input: "Java is easy" Output: "avaJ si ysae" */
+    public static void main(String []args){
+        String str ="Java is easy";
+        
+    }
+}
--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -1,4 +1,17 @@
 package string;
 
 public class ReverseString {
+    /*1.Reverse a String Input: "hello" Output: "olleh" */
+    public static void main(String args[]){
+        String input ="hello";
+        String str="";
+        for(int i=input.length()-1;i>=0;i--){
+            str=str+input.charAt(i);
+        }
+        System.out.println(str);
+
+        // using stream
+        String res=input.chars().mapToObj(c->String.valueOf((char)c)).reduce("",(a,b)->b+a);
+        System.out.println(res);
+    }
 }
