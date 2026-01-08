500d3d2514181ef0e80f964ed838cdae38b6433a
Shubham <shubhaws74@gmail.com>
2026-01-09 00:00:31 +0530

    latest commit

--- src/string/ReverseEachWord.java
+++ src/string/ReverseEachWord.java
@@ -4,7 +4,12 @@ import java.util.*;
 public class ReverseEachWord {
     /*2.Reverse Each Word in a String Input: "Java is easy" Output: "avaJ si ysae" */
     public static void main(String []args){
+        StringBuilder sb = new StringBuilder();
         String str ="Java is easy";
-        
+        for(String s:str.trim().split("\\+")){
+            sb.append(new StringBuilder(s).reverse());
+        }
+        System.out.println(sb.toString());
+
     }
 }
